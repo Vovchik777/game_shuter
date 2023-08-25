9f0be0aacd39c91773349949caa11c371205a4f3
real.tesla@gmail.com <real.tesla@gmail.com>
2023-08-25 23:09:42 +0300

    123

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-import pygame,random
+import pygame, random
 
 import events
 from events import Events
@@ -7,10 +6,15 @@ from colors import *
 from config import *
 from config_spaceship import *
 
+bullets = []
+
 shipX = 20
 shipY = 10
 speed = 10
 
+bulletX = shipX + 34
+bulletY = shipY / 2
+bulletspeed = 2
 
 pygame.init()
 
@@ -22,25 +26,33 @@ clock = pygame.time.Clock()
 
 background_image = pygame.image.load('images/fon.jpg')
 spaceship_image = pygame.image.load('images/spaceship.png')
+bullet_image = pygame.image.load('images/pula2.png')
 
 running = True
 
-while running: #event.running:
+while running:  # event.running:
     pygame.display.flip()
     # event.process()
-    screen.blit(background_image,(0,0))
-    screen.blit(spaceship_image,(shipX,shipY))
+    screen.blit(background_image, (0, 0))
+    screen.blit(spaceship_image, (shipX, shipY))
     clock.tick(FPS)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     if pygame.key.get_pressed()[pygame.K_s]:
-        if shipY < HEIGHT-34:
+        if shipY < HEIGHT - 34:
             shipY += speed
     elif pygame.key.get_pressed()[pygame.K_w]:
         if shipY > 0:
             shipY -= speed
-        # elif event.type == pygame.KEY:
-        #     if event.key == pygame.K_s:
-        #         shipY += speed
+    if pygame.key.get_pressed()[pygame.K_SPACE]:
+        for i in range(20):
+            bullets.append({f"bullet{+i}",shipX+34, shipY/2 })
+            print(bullets)
+            # screen.blit(bullet_image,(bulletX,bulletY))
+            # bulletX+=bulletspeed
+    # elif event.type == pygame.KEY:
+    #     if event.key == pygame.K_s:
+    #         shipY += speed
