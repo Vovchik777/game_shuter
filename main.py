9e5d5d4bd980d0d9a43a54dc9431455ed4aec3ca
real.tesla@gmail.com <real.tesla@gmail.com>
2023-08-25 21:35:46 +0300

    добавил ксомолет

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ pygame.display.set_caption("шутер")
 clock = pygame.time.Clock()
 
 background_image = pygame.image.load('images/fon.jpg')
+spaceship_image = pygame.image.load('images/spaceship.png')
 
 
 while event.running:
     event.process()
     screen.blit(background_image,(0,0))
+    screen.blit(spaceship_image,(20,100))
     pygame.display.flip()
+
+    clock.tick(FPS)
